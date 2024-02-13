e41f63d3613a9a4f7c138955e4e0b4f2fb1fddb0
miguelangellujan <miguel.lujan@edu.uah.es>
2024-02-13 20:02:52 +0100

    prueba

---
+++ src/Date.java
@@ -0,0 +1,36 @@
+public class Date {
+    private int day;
+    private int month;
+    private int year;
+    public Date(int d, int m, int y) {
+        day = d;
+        month = m;
+        year = y;
+    }
+    public int getDay() {
+        return day;
+    }
+    public int getMonth() {
+        return month;
+    }
+    public int getYear() {
+        return year;
+    }
+    public void setDay(int newday) {
+        this.day = newday;
+    }
+    public void setMonth(int newmonth) {
+        this.month = newmonth;
+    }
+    public void setYear(int newyear) {
+        this.year = newyear;
+    }
+    public void setDate(int newday, int newmonth, int newyear) {
+        this.day = newday;
+        this.month = newmonth;
+        this.year = newyear;
+    }
+    public String toString() {
+        return day + "/" + month + "/" + year;
+    }
+}
