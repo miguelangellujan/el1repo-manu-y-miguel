a11a8a06101b7e17ca1567ee4644755438bcdfb0
miguelangellujan <miguel.lujan@edu.uah.es>
2024-02-23 16:43:54 +0100

    prueba

---
+++ src/es/uah/matcomp/mp/e1/ejerciciosclases/ejercicio8/Ball.java
@@ -0,0 +1,63 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases.ejercicio8;
+public class Ball {
+    private float x;
+    private float y;
+    private float xDelta;
+    private float yDelta;
+    private int radius;
+    public Ball(float x,float y,int radius,float xDelta,float yDelta){
+        this.x=x;
+        this.y=y;
+        this.radius=radius;
+        this.yDelta=yDelta;
+        this.xDelta=xDelta;}
+
+    public float getX() {
+        return x;
+    }
+    public void setX(float x) {
+        this.x = x;
+    }
+    public void setY(float y) {
+        this.y = y;
+    }
+    public float getY() {
+        return y;
+    }
+    public int getRadius() {
+        return radius;
+    }
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public float getxDelta() {
+        return xDelta;
+    }
+
+    public void setyDelta(float yDelta) {
+        this.yDelta = yDelta;
+    }
+
+    public float getyDelta() {
+        return yDelta;
+    }
+
+    public void setxDelta(float xDelta) {
+        this.xDelta = xDelta;
+    }
+    public void move (){
+        x+=xDelta;
+        y+=yDelta;
+    }
+    public void reflectHorizontal(){
+        xDelta=-xDelta;
+    }
+    public void reflectVertical(){
+        yDelta=-yDelta;
+    }
+    public String toString(){
+        return "Ball[("+x+","+y+"),speed=("+xDelta+","+yDelta+")]";
+    }
+}
+
